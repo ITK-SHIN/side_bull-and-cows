598567e9ccf28f362c8481ccfd987d6f928100dc
신상우 <bano112@naver.com>
2023-09-01 00:58:18 +0900

    정답 맞추기 기능 개발완료

--- src/App.js
+++ src/App.js
@@ -1,15 +1,59 @@
+import { useState, useEffect } from 'react';
 import './App.css';
 import generateRandomNumber from './random';
 
 function App() {
-  console.log(generateRandomNumber());
+  const [randomNumber, setRandomNumber] = useState(generateRandomNumber());
+
+  const [answer, setAnswer] = useState('');
+
+  useEffect(() => {
+    console.log(randomNumber);
+  }, [randomNumber]);
+
+  const handleAnswerChanged = e => {
+    setAnswer(e.target.value);
+  };
+
+  const handleSubmit = () => {
+    // 스트라이크 , 볼, 정답 유무
+    const answers = answer.split('').map(item => Number(item));
+
+    const { strike, ball } = randomNumber.reduce(
+      (prev, cur, index) => {
+        // 같은 자리에 같은 수가 존재하면 스트라이크
+        if (answers[index] === cur) {
+          return {
+            ...prev,
+            strike: prev.strike + 1,
+          };
+        }
+
+        // 다른 자리에 수가 존재하면 볼
+        if (answers.includes(cur)) {
+          return {
+            ...prev,
+            ball: prev.ball + 1,
+          };
+        }
+
+        return prev;
+      },
+      {
+        strike: 0,
+        ball: 0,
+      }
+    );
+    console.log(strike, ball);
+  };
+
   return (
     <div className="App">
       <h1>숫자 야구 게임</h1>
-      <header className="header">----</header>
+      <header className="header">{randomNumber}</header>
       <section>
-        <input />
-        <button>맞춰보기</button>
+        <input type="text" value={answer} onChange={handleAnswerChanged} />
+        <button onClick={handleSubmit}>맞춰보기</button>
       </section>
       <h2>기록</h2>
       <ol>
